cd36f5136814da7a1fcd3b457dbb1c70a493869f
Minillorsh1 <minillorsh@gmail.com>
2025-05-26 19:34:12 -0400

    primer commit

---
+++ src/main/java/com/evaluacion/microEvaluacion/MicroEvaluacionApplication.java
@@ -0,0 +1,13 @@
+package com.evaluacion.microEvaluacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroEvaluacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroEvaluacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/evaluacion/microEvaluacion/MicroEvaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluacion.microEvaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroEvaluacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
